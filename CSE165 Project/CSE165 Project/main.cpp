b1c73c48cd5b43637cf4817d3d7d05b2d4bbfab9
Andrew Hom <andrewhom77@gmail.com>
2024-04-23 23:40:36 -0700

    Added SDL_image library

--- CSE165 Project/CSE165 Project/main.cpp
+++ CSE165 Project/CSE165 Project/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include <iostream>
 
 
@@ -8,11 +9,14 @@
 //=============================================================
 
 
-void render_loop()
+void render_loop(SDL_Renderer *renderer)
 {
-	SDL_SetRenderDrawColor(renderer, 14, );
-
-	while()
+	while (1)
+	{
+		SDL_SetRenderDrawColor(renderer, 14, 4, 60, 212); // Set window background color
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}	
 }
 
 
@@ -50,13 +54,13 @@ int main(int argc, char** argv)
 {
 	SDLINIT();
 	
-	SDL_Window *SDL_GameWindow = SDL_CreateWindow("Game Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL | SDL_WINDOW_MOUSE_GRABBED | SDL_WINDOW_KEYBOARD_GRABBED);
+	SDL_Window *SDL_GameWindow = SDL_CreateWindow("Game Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL);
 	SDL_Renderer *renderer = SDL_CreateRenderer(SDL_GameWindow, -1, 0); // (Window, -1 is the first rendering driver to support flags, 0 or flags | together)
 	//Need to create loop to keep everything going
-	SDL_WaitEvent();
+	
 
-
-	render_loop();
+	
+	render_loop(renderer);
 
 	return 0;
 }
