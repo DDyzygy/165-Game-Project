53b59d1bd915a2e006e3a434ad77ad6536ec6d54
Andrew Hom <andrewhom77@gmail.com>
2024-04-24 00:13:34 -0700

    Started thinking about event handling, added SDL2 dll to project directory

--- CSE165 Project/CSE165 Project/main.cpp
+++ CSE165 Project/CSE165 Project/main.cpp
@@ -13,12 +13,51 @@ void render_loop(SDL_Renderer *renderer)
 {
 	while (1)
 	{
-		SDL_SetRenderDrawColor(renderer, 14, 4, 60, 212); // Set window background color
+		SDL_SetRenderDrawColor(renderer, 28, 9, 41, 212); // Set window background color
 		SDL_RenderClear(renderer);
+		keyboard_func; // Replace this later
 		SDL_RenderPresent(renderer);
 	}	
 }
 
+void keyboard_func(void) // Could just make this a class
+{
+	SDL_Event event;
+
+	switch (event.type)
+	{
+	/*
+	case 'w':
+	{
+		std::cout << "Up" << std::endl;
+		break;
+	}
+
+	case 'a':
+	{
+		std::cout << "Left" << std::endl;
+		break;
+	}
+
+	case 's':
+	{
+		std::cout << "Down" << std::endl;
+		break;
+	}
+
+	case 'd':
+	{
+		std::cout << "Right" << std::endl;
+		break;
+	}
+
+	// Exit on escape key press
+	case '\x1B':
+	{
+		break;
+	}
+	*/
+	}
 
 void SDLINIT(void)
 {
@@ -54,7 +93,7 @@ int main(int argc, char** argv)
 {
 	SDLINIT();
 	
-	SDL_Window *SDL_GameWindow = SDL_CreateWindow("Game Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL);
+	SDL_Window *SDL_GameWindow = SDL_CreateWindow("Game Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1600, 900, SDL_WINDOW_OPENGL);
 	SDL_Renderer *renderer = SDL_CreateRenderer(SDL_GameWindow, -1, 0); // (Window, -1 is the first rendering driver to support flags, 0 or flags | together)
 	//Need to create loop to keep everything going
 	
